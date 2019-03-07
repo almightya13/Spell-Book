dcbfe7b62f7b79f1d422d6d95f9834760f4d4eb8
almightya13 <44871190+almightya13@users.noreply.github.com>
2019-03-07 09:27:34 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#trick1st").click(function(){
+$(".spiderman").fadeToggle();
+});
+
+$("#trick2nd").click(function(){
+$(".groot").slideToggle();
+});
+
+$("#trick3rd").click(function(){
+$(".dr").toggle();
+});
